5fc22d253fa2735f48db99c0daf0fed05295134c
Stefan Staub <stefan.staub@pta.ch>
2020-09-16 15:53:01 +0200

    fixed layout issues on main activity

--- app/src/main/java/ch/innodrive/copyscan/MainActivity.java
+++ app/src/main/java/ch/innodrive/copyscan/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,7 +24,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnConnect;
-    private Button btnDisconnect;
+    private ImageButton btnDisconnect;
     private IntentIntegrator qrScan;
     private TextView channel;
     private View connectedView;
